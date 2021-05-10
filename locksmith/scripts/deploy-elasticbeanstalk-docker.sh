aa8ba2217d1dd857332d9f0cf902cd9b416dc3f3
Julien Genestoux <julien.genestoux@gmail.com>
2021-05-10 17:03:58 -0400

    refactoring deploy script for locksmith

--- locksmith/scripts/deploy-elasticbeanstalk-docker.sh
+++ locksmith/scripts/deploy-elasticbeanstalk-docker.sh
@@ -1,60 +1,36 @@
-application=$1
-environment=$2
-db_username=$3
-db_password=$4
-db_name=$5
-db_hostname=$6
-node_env=$7
-is_forked_pr=$8
-build_id=$9
-message="${10}"
-stripe_secret=${11}
-purchaser_credentials=${12}
-web3_provider_host=${13}
-unlock_contract_address=${14}
-graphql_base_url=${15}
-metadata_host=${16}
-default_network=${17}
+
+# We pass only the relevent env vars, which are prefixed with the service name, uppercased
+# UNLOCK_APP_X will be passed to the container for tests in unlock_app as X.
+UPCASE_SERVICE="LOCKSMITH"
+TARGET="BEANSTALK"
+NODE_ENV="production"
+
+ENV_PREFIX="STAGING_"
+DEFAULT_NETWORK="4"
+if [ "$ENV_TARGET" = "prod" ]; then
+  ENV_PREFIX="PROD_"
+  DEFAULT_NETWORK="1"
+fi
+
+ENV_VARS_PREFIX="${UPCASE_SERVICE//-/_}_${TARGET^^}_$ENV_PREFIX"
+ENVIRONMENT_NAME="$ENV_VARS_PREFIX"ENVIRONMENT
+APPLICATION_NAME="$ENV_VARS_PREFIX"APPLICATION
 
 
-function check_is_forked_pr()
-{
-    if [ $is_forked_pr = "true" ]; then
-        echo "Skipping deployment because this is a pull request from a forked repository."
-        exit 0
-    fi
-}
+ENV_VARS=`env | grep "^$ENV_VARS_PREFIX" | awk '{print " ",$1}' ORS='' | sed -e "s/$ENV_VARS_PREFIX//g"`
 
-function check_if_locksmith_changed()
-{
-    # latest commit
-    LATEST_COMMIT=$(git rev-parse HEAD)
-    # latest commit where path/to/folder1 was changed
-    LAST_LOCKSMITH_COMMIT=$(git log -1 --format=format:%H --full-diff ./locksmith)
 
-    if [ $LAST_LOCKSMITH_COMMIT != $LATEST_COMMIT ];then
-        echo "No changes to Locksmith, no need to deploy"
-        exit 0
-    fi
-}
+function deploy() {
 
-function deploy()
-{
-    environment_name=$1
-
-    if eb status ${environment_name}; then
-        eb setenv DB_USERNAME=${db_username} DB_PASSWORD=${db_password} DB_NAME=${db_name} DB_HOSTNAME=${db_hostname} NODE_ENV=${node_env} STRIPE_SECRET=${stripe_secret} PURCHASER_CREDENTIALS=${purchaser_credentials} WEB3_PROVIDER_HOST=${web3_provider_host} UNLOCK_CONTRACT_ADDRESS=${unlock_contract_address} GRAPHQL_BASE_URL=${graphql_base_url} METADATA_HOST=${metadata_host} DEFAULT_NETWORK=${default_network}
-        eb deploy ${environment_name} --label locksmith-${build_id} --message "${message:0:199}" --timeout 10
+    eb setenv $ENV_VARS NODE_ENV=$NODE_ENV DEFAULT_NETWORK=$DEFAULT_NETWORK
+    if eb status ${!ENVIRONMENT_NAME}; then
+        eb deploy ${!ENVIRONMENT_NAME} --label locksmith-${build_id} --message "${message:0:199}" --timeout 10 --verbose
     else
-        eb create ${environment_name} --envvars DB_USERNAME=${db_username},DB_PASSWORD=${db_password},DB_NAME=${db_name},DB_HOSTNAME=${db_hostname},NODE_ENV=${node_env},STRIPE_SECRET=${stripe_secret},PURCHASER_CREDENTIALS=${purchaser_credentials},WEB3_PROVIDER_HOST=${web3_provider_host},UNLOCK_CONTRACT_ADDRESS=${unlock_contract_address},GRAPHQL_BASE_URL=${graphql_base_url},METADATA_HOST=${metadata_host},DEFAULT_NETWORK=${default_network} --elb-type classic
+        eb create ${!ENVIRONMENT_NAME} --elb-type classic --debug
     fi
-
 }
-
-check_is_forked_pr
 
 cd locksmith
 
-eb init ${application} -p docker --region us-east-1
-
-deploy ${environment}
+eb init ${!APPLICATION_NAME} -p docker --region us-east-1
+deploy ${!ENVIRONMENT_NAME}
